4a3504782f2158fbb72474632ad60a3a9d813695
Lotlin <morozova.olesya2020@list.ru>
2023-02-03 00:39:51 +0300

    added stop melodies for diferent statuses

--- js/control.js
+++ js/control.js
@@ -1,18 +1,42 @@
 import { state } from './state.js';
-import { startTimer } from './timer.js';
+import { showTime, startTimer } from './timer.js';
 
 const btnStart = document.querySelector('.control__btn_start');
+const btnStop = document.querySelector('.control__btn_stop');
+const navigationBtns = document.querySelectorAll('.navigation__btn');
+
+export const changeActiveBtn = (dataUse) => {
+    for (let i = 0; i < navigationBtns.length; i++) {
+        if (navigationBtns[i].dataset.use === dataUse) {
+            navigationBtns[i].classList.add('navigation__btn_active');
+        } else {
+            navigationBtns[i].classList.remove('navigation__btn_active');
+        }
+    }
+}
+
+const start = () => {
+    if (state.isActive) {
+        clearTimeout(state.timerId);
+        state.isActive = false;
+        btnStart.textContent = 'Старт';
+    } else {
+        state.isActive = true;
+        btnStart.textContent = 'Пауза';
+        startTimer();
+    }
+}
+
+const stop = () => {
+    clearTimeout(state.timerId);
+    state.isActive = false;
+    btnStart.textContent = 'Старт';
+    state.timeLeft = state[state.status] * 60;
+    showTime(state.timeLeft);
+}
 
 export const initControl = () => {
-    btnStart.addEventListener('click', () => {
-        if (state.isActive) {
-            clearTimeout(state.timerId);
-            state.isActive = false;
-            btnStart.textContent = 'Старт';
-        } else {
-            state.isActive = true;
-            btnStart.textContent = 'Пауза';
-            startTimer();
-        }
-    });
+    btnStart.addEventListener('click', start);
+    btnStop.addEventListener('click', stop)
+    showTime(state.timeLeft);
 }
